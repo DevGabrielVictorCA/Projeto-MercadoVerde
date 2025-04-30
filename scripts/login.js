9da9a4bd8eed52694ec30b30707cb59842dd8fe6
Gabriel Cardoso <gabrielvictorcardosopeace@gmail.com>
2025-04-30 14:13:14 -0300

    fix: correção de bug no botão 'Entrar' da página index

--- scripts/login.js
+++ scripts/login.js
@@ -1,20 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const form = document.getElementById("register-form");
-    const passwordInput = document.getElementById("password");
-    const confirmPasswordInput = document.getElementById("confirm-password");
+    const loginForm = document.getElementById("login-form");
 
-    form.addEventListener("submit", (e) => {
-        e.preventDefault();
-        
-        const password = passwordInput.value;
-        const confirmPassword = confirmPasswordInput.value;
-
-        if (password !== confirmPassword) {
-            alert("As senhas não coincidem. Por favor, verifique.");
-            return;
-        }
-
-        alert("Cadastro realizado com sucesso!");
-        form.reset(); // Limpa os campos do formulário
+    loginForm.addEventListener("submit", (e) => {
+        e.preventDefault(); 
+        window.location.href = "home.html";  
     });
 });
